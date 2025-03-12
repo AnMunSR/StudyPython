e9f7f753eab7c9aeee72cf600b3eb619cc7ecdb3
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:55:35 +0700

    Add files via upload

---
+++ BaiTapLenhDieuKhienCoBan/Bai14.py
@@ -0,0 +1,12 @@
+# Bai tap lenh dieu khien co ban
+'''
+Bai14: Nhap vao so nguyen duong a, neu a la so chan thi in ra 
+day la so chan, nguoc lai in ra day la so le
+'''
+
+import math
+a = int(input("Nhap vao so a: "))
+if a % 2 == 0:
+    print("Day la so chan")
+else:
+    print("Day la so le")
