c2fe1ea9ac94517c52b1a0e8674042992106e7e9
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:01:06 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai2.py
@@ -0,0 +1,7 @@
+# Bai toan nhap lieu va toan tu co ban:
+# Bai2: Nhap vao so n, hay mu 2 roi chia cho 3, sau do in ket qua ra man hinh
+
+import math
+n = int(input("Nhap vao so n = "))
+ketQua = pow(n,2)/3
+print("Ket qua: ", ketQua)
