f6178b47801ffa471a60865d5be5fc4b49dbf79c
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:53:08 +0700

    Add files via upload

---
+++ BaiTapLenhDieuKhienCoBan/Bai13.py
@@ -0,0 +1,10 @@
+# Bai tap lenh dieu khien co ban
+'''
+Bai13: Nhap vao so nguyen duong a, neu a lon hon 10 thi 
+in ra day la so lon hon 10
+'''
+
+import math
+a = int(input("Nhap vao so a: "))
+if a > 10:
+    print("Day la so lon hon 10")
