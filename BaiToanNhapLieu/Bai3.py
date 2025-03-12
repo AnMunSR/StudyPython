f0865179e84fc0c5336e352a2db02925e0104d06
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:05:00 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai3.py
@@ -0,0 +1,7 @@
+# Bai toan nhap lieu va toan tu co ban:
+# Bai3: Nhap vap nhiet do C, in ra nhiet do F
+
+import math
+C = int(input("Nhap vao nhiet do (do C): "))
+F = (2 * 9/5) + 32
+print("Nhiet do: ",F," (do F)")
