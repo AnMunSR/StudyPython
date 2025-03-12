a11a54dce9749077df6a84d7d73a3034fb0285d6
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:07:51 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai4.py
@@ -0,0 +1,9 @@
+# Bai toan nhap lieu va toan tu co ban:
+# Bai4: Nhap vao mot so nguyen a, neu a chia het cho 3 thi in ra True, nguoc lai in ra False
+
+import math
+a = int(input("Nhap vao so nguyen a: "))
+if a % 3 == 0:
+    print("True")
+else:
+    print("False")
