8b08b92c92f88dcb735677cf7ff5319bb88f534d
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:31:02 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai10.py
@@ -0,0 +1,9 @@
+# Bai toan nhap lieu va toan tu co ban:
+'''
+Bai10: Nhap vao luong thang nay nhan duoc, ta phai dua cho vo 90%
+so tien luong do. Hay in ra luong ta giu lai
+'''
+
+import math
+a = float(input("Nhap vao so luong a: "))
+print("So luong con lai: ", a*0.1)
