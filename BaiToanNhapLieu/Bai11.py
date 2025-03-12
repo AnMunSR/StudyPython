1953b556f4d55a0fdec472ff3cb04117cc38b398
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:43:00 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai11.py
@@ -0,0 +1,11 @@
+# Bai toan nhap lieu va toan tu co ban:
+'''
+Bai11: Nhap vao 3 so a, b, c. In ta ket qua tong cua 3 so do
+'''
+
+import math
+a = float(input("Nhap vao so a: "))
+b = float(input("Nhap vao so b: "))
+c = float(input("Nhap vao so c: "))
+tong = a + b + c
+print("Tong 3 so {0}, {1}, {2} la {3} ".format(a ,b ,c ,tong))
