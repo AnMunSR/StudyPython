e1ef138fa156dfbc4fc3aafa248f43bb84053e89
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 23:25:54 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai8.py
@@ -0,0 +1,12 @@
+# Bai toan nhap lieu va toan tu co ban:
+'''
+Bai8: Nhap vao so thuc a, kiem tra xem a co phai so nguyen hay khong
+neu co in ra True, nguoc lai in ra False 
+'''
+
+import math
+a = float(input("Nhap vao so thuc a: "))
+if a.is_integer():
+    print("True")
+else:
+    print("False")
