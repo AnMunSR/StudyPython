17ae56103d7a207ba4985b111e2a80b3c23efe65
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-12 22:57:35 +0700

    Add files via upload

---
+++ BaiToanNhapLieu/Bai1.py
@@ -0,0 +1,6 @@
+# Bai toan nhap lieu va toan tu co ban:
+# Bai1: Nhap vao so n, hay nhan n len cho 3, roi cong 1 sau do in ket qua  ra man hinh
+
+n = int(input("Nhap vao so n: "))
+tong = n * 3 + 1
+print("Ket qua: ",tong)
