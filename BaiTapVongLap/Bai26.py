4bb312003f4e2e203af3112f3f7492fd5609c3fc
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-14 00:49:28 +0700

    Add files via upload

---
+++ BaiTapVongLap/Bai26.py
@@ -0,0 +1,10 @@
+# Bai tap vong lap for
+# Bai 26: Nhap vao so nguyen duong a, in ra bang cuu chuong cua a
+
+import math
+from datetime import datetime
+
+a = int(input("Nhap vao so a = "))
+print("Bang cuu chuong cua ",a)
+for i in range(1,11):
+    print(a," x ",i," = ", a * i)
