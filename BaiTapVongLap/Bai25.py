fc0faa4853c62e6b398d4075f0fff0951cce833c
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-14 00:46:49 +0700

    Add files via upload

---
+++ BaiTapVongLap/Bai25.py
@@ -0,0 +1,9 @@
+# Bai tap vong lap for
+# Bai 25: In cac so chan chia het cho 3 be hon 100
+
+import math
+from datetime import datetime
+
+for i in range(0,100):
+    if (i % 3 == 0) and (i % 2 == 0):
+        print(i, end = ' ')
