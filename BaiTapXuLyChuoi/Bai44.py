48edf358de49adf2d489f5cae09e82d82ce3a091
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-15 00:59:21 +0700

    Add files via upload

---
+++ BaiTapXuLyChuoi/Bai44.py
@@ -0,0 +1,16 @@
+# Bai tap xu ly chuoi
+# Bai 44: Nhap vao mot chuoi, hay in ra tu dau tien trong chuoi
+
+
+
+import math
+from datetime import datetime
+
+s = input("Nhap chuoi s = ")
+i = 0
+while True:
+    if (s[i] == " "):
+        break
+    print(s[i], end = '')
+    i += 1
+
