987ce91895d5b365228a91fe2ec9e5311a4576b0
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-14 15:59:04 +0700

    Add files via upload

---
+++ BaiTapVongLap/Bai31.py
@@ -0,0 +1,19 @@
+# Bai tap vong lap for
+# Bai 31: Nhap vao so nguyen duong a va b, in toan bo uoc chung cua a va b
+
+
+
+import math
+from datetime import datetime
+
+# Ham tim toan bo uoc chung cua a va b
+def uoc(a, b):
+    dich = min(a,b)
+    for i in range (1 , dich + 1):
+        if (a % i == 0) and (b % i == 0):
+            print(i, end = ' ')
+
+# Nhap du lieu dau vao
+a = int(input("Nhap so nguyen a = "))
+b = int(input("Nhap so nguyen b = "))
+uoc(a,b)
