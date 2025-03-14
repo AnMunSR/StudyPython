ba1dae904711ca2a8e27aab9db7bcd033db1dccd
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-15 00:53:49 +0700

    Add files via upload

---
+++ BaiTapXuLyChuoi/Bai43.py
@@ -0,0 +1,12 @@
+# Bai tap xu ly chuoi
+# Bai 43: Nhap vao mot chuoi, hay dem xem trong chuoi co bao nhieu tu
+#(quy dinh chuoi la chuoi khong co ky tu dac biet, khong so, khong dau cau, chi co ky tu chu va khoang trang)
+
+
+
+import math
+from datetime import datetime
+
+s = input("Nhap chuoi s = ")
+s1 = s.split()
+print("So chuoi con: ", len(s1))
