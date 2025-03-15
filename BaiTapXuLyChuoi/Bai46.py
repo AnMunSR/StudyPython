a54d7136b065e883f7a739d05de42ead47380a49
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-15 23:19:36 +0700

    Add files via upload

---
+++ BaiTapXuLyChuoi/Bai46.py
@@ -0,0 +1,17 @@
+# Bai tap xu ly chuoi
+# Bai 46: Nhap vao mot chuoi, hay dem xem trong chuoi co bao nhieu ki tu in hoa
+#bao nhieu ky tu in thuong, bao nhieu ki tu so
+
+
+s = input("Nhap vao chuoi s: ")
+countHoa = 0
+countThuong = 0
+countSo = 0
+for i in range(0, len(s)):
+    if s[i].islower():
+        countHoa += 1
+    elif s[i].isupper():
+        countThuong += 1
+    elif s[i].isdigit():
+        countSo += 1
+print("So ky tu thuong, hoa, so lan luot la: ", countThuong, countHoa, countSo)
