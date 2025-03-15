750091de95a74e925f595dc5662af87b07201309
AnMunSR <tranthianhnguyet259@gmail.com>
2025-03-15 23:23:44 +0700

    Add files via upload

---
+++ BaiTapXuLyChuoi/Bai48.py
@@ -0,0 +1,9 @@
+# Bai tap xu ly chuoi
+# Bai 48: Nhap vao mot chuoi, hay tach toan bo ky tu so trong hcuoi ra roi tinh tong cua chung
+
+s = input("Nhap vao chuoi s: ")
+tong = 0
+for i in range(0, len(s)):
+    if s[i].isdigit():
+        tong += int(s[i])
+print("Tong cua cac chu so trong chuoi la: ", tong)
