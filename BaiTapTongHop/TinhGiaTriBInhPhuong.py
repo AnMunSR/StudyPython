fe804975ba5a6731e6780dfe2afe8286e7a540fb
Trần Thị Ánh Nguyệthị Ánh Nguyệt <tranthianhnguyet259@gmail.com>
2025-05-04 22:16:36 +0700

    Add files via upload

---
+++ BaiTapTongHop/TinhGiaTriBInhPhuong.py
@@ -0,0 +1,8 @@
+'''
+Viet mot method tinh gia tri binh phuong cua mot so
+Su dung toan tu **
+'''
+
+# Nhap du lieu dau vao
+n = int(input("Nhap vao so can tinh binh phuong: "))
+print("Binh phuong cua {0} la: {1}".format(n,n**2))
